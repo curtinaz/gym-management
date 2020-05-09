9208e62d9f000ff41eceff8ecdb2dc3755a58702
curtinaz <curtinazif@gmail.com>
2020-05-09 01:31:02 -0300

    create passa a gerar uma página
    agora o create passa a gerar uma página com seu id (parâmetro). por exemplo: /instructors/{id}

--- instructors.js
+++ instructors.js
@@ -1,6 +1,28 @@
 const fs = require('fs')
 const data = require('./data.json')
+const {age} = require('./utils.js')
 
+// show
+
+exports.show = function (req, res) {
+
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor) {
+        return id == instructor.id
+    })
+
+    if (!foundInstructor) return res.send ("Instructor not found!")
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
+    }
+
+    return res.render('./instructors/show', { instructor })
+}   
 
 // create
 exports.post = function (req, res) {
@@ -13,10 +35,21 @@ exports.post = function (req, res) {
         }
     }
     
-    req.body.birth = Date.parse(req.body.birth)
-    req.body.created_at = Date.now()
+    let { avatar_url, name, birth, gender, services } = req.body
 
-    data.instructors.push(req.body) // [{...},{...}]
+    birth = Date.parse(req.body.birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        gender,
+        services,
+        birth,
+        created_at
+    }) // [{...},{...}]
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
     if (err) return res.send("Write File error!")
