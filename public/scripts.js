fc25a7041fc4bb01ba65b7a1ad193dd1a639c2af
curtinaz <curtinazif@gmail.com>
2020-05-13 22:40:16 -0300

    fixed menuItems
    Consertado um problema que fazia o atributo do menu receber a classe ".active" somente quando o 'path' não tinha nada além do caminho. Agora basta o valor do href conter qualquer parte do path para dar a classe ".active" ao atributo.

--- public/scripts.js
+++ public/scripts.js
@@ -2,7 +2,7 @@ const currentPage = window.location.pathname
 const menuItems = document.querySelectorAll("header .links a")
 
 for (item of menuItems) {
-    if (currentPage == item.getAttribute("href")) {
+    if (currentPage.includes(item.getAttribute("href"))) {
         item.classList.add("active")
     }
 }
